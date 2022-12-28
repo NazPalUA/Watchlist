c8b0dd294add66fe63408f81b2250f1e05fbef91
NazPalUA <104204125+NazPalUA@users.noreply.github.com>
2022-12-28 18:58:16 +0200

    created Welcome component

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+import checkmarkWhiteIcon from "../images/checkmark_white_icon.svg"
+import ribbonIcon from "../images/ribbon_icon.svg"
+import './Welcome.scss'
+
+export default function Welcome() {
+    return (
+        <div className="welcome">
+            <h1 className="welcome__title">
+                Welcome to <span>Watchlists</span>
+            </h1>
+            <p className="welcome__paragraph">
+                Browse movies, add them to watchlists and share them with friends. 
+            </p>
+            <p className="welcome__paragraph welcome__paragraph_2">
+                Just click the<img 
+                    className="welcome__add-icon"
+                    src={ribbonIcon}
+                    alt="add icon"
+                />to add a movie, the poster to see more details or<img 
+                    className="welcome__watched-icon"
+                    src={checkmarkWhiteIcon}
+                    alt="watched icon"
+                />to mark the movie as watched.
+            </p>
+
+        </div>
+    )
+}
