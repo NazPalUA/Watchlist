8a77088d52ea5be6a9e8f16e18497d977406c6dd
NazPalUA <104204125+NazPalUA@users.noreply.github.com>
2023-01-07 00:09:15 +0200

    created history page

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,8 +1,30 @@
 import React from "react"
+import MovieCard from "../components/MovieCard"
 import './History.scss'
 
 export default function History(props) {
     return (
-        <div className={`history ${props.className}`}>History Page</div>
+        <div className={`history ${props.className}`}>
+            <button className="history__clear-btn">Clear history</button>
+            <ul className="history__list">
+                <li className="history__item"><MovieCard className="history__movie-card" /></li>
+                <li className="history__item"><MovieCard className="history__movie-card" /></li>
+                <li className="history__item"><MovieCard className="history__movie-card" /></li>
+                <li className="history__item"><MovieCard className="history__movie-card" /></li>
+                <li className="history__item"><MovieCard className="history__movie-card" /></li>
+                <li className="history__item"><MovieCard className="history__movie-card" /></li>
+                <li className="history__item"><MovieCard className="history__movie-card" /></li>
+                <li className="history__item"><MovieCard className="history__movie-card" /></li>
+                <li className="history__item"><MovieCard className="history__movie-card" /></li>
+                <li className="history__item"><MovieCard className="history__movie-card" /></li>
+                <li className="history__item"><MovieCard className="history__movie-card" /></li>
+                <li className="history__item"><MovieCard className="history__movie-card" /></li>
+                <li className="history__item"><MovieCard className="history__movie-card" /></li>
+                <li className="history__item"><MovieCard className="history__movie-card" /></li>
+                <li className="history__item"><MovieCard className="history__movie-card" /></li>
+                <li className="history__item"><MovieCard className="history__movie-card" /></li>
+                <li className="history__item"><MovieCard className="history__movie-card" /></li>
+            </ul>
+        </div>
     )
 }
