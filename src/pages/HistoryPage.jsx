c25e14f68b75105cf4e91729e3d8c2c4b2cc8215
NazPalUA <104204125+NazPalUA@users.noreply.github.com>
2023-02-02 18:05:45 +0200

    HistoryPage.jsx refactoring

--- src/pages/HistoryPage.jsx
+++ src/pages/HistoryPage.jsx
@@ -1,79 +1,29 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { nanoid } from "nanoid"
 import MovieCard from "../components/MovieCard"
 import './HistoryPage.scss'
 
 export default function HistoryPage(props) {
+
+    const historyList = []
+    for (let i = 0; i<17; i++) {
+        historyList.push(
+            <li className="history-page__item"
+                key={nanoid()}
+            >
+                <Link to="/movie-page" className="history-page__link">
+                    <MovieCard className="history-page__movie-card" />
+                </Link>
+            </li>
+        )
+    }
+
     return (
         <div className={`history-page ${props.className}`}>
             <button className="history-page__clear-btn">Clear history</button>
             <ul className="history-page__list card-grid">
-                <li className="history-page__item">
-                    <Link to="/movie-page" className="history-page__link">
-                        <MovieCard className="history-page__movie-card" />
-                    </Link>
-                </li>
-
-                <li className="history-page__item">
-                    <Link to="/movie-page" className="history-page__link">
-                        <MovieCard className="history-page__movie-card" />
-                    </Link>
-                </li>
-
-                <li className="history-page__item">
-                    <Link to="/movie-page" className="history-page__link">
-                        <MovieCard className="history-page__movie-card" />
-                    </Link>
-                </li>
-
-                <li className="history-page__item">
-                    <Link to="/movie-page" className="history-page__link">
-                        <MovieCard className="history-page__movie-card" />
-                    </Link>
-                </li>
-
-                <li className="history-page__item">
-                    <Link to="/movie-page" className="history-page__link">
-                        <MovieCard className="history-page__movie-card" />
-                    </Link>
-                </li>
-
-                <li className="history-page__item">
-                    <Link to="/movie-page" className="history-page__link">
-                        <MovieCard className="history-page__movie-card" />
-                    </Link>
-                </li>
-
-                <li className="history-page__item">
-                    <Link to="/movie-page" className="history-page__link">
-                        <MovieCard className="history-page__movie-card" />
-                    </Link>
-                </li>
-
-                <li className="history-page__item">
-                    <Link to="/movie-page" className="history-page__link">
-                        <MovieCard className="history-page__movie-card" />
-                    </Link>
-                </li>
-
-                <li className="history-page__item">
-                    <Link to="/movie-page" className="history-page__link">
-                        <MovieCard className="history-page__movie-card" />
-                    </Link>
-                </li>
-
-                <li className="history-page__item">
-                    <Link to="/movie-page" className="history-page__link">
-                        <MovieCard className="history-page__movie-card" />
-                    </Link>
-                </li>
-
-                <li className="history-page__item">
-                    <Link to="/movie-page" className="history-page__link">
-                        <MovieCard className="history-page__movie-card" />
-                    </Link>
-                </li>
-
+                {historyList}
             </ul>
         </div>
     )
