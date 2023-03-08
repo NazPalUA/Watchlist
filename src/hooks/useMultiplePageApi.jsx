764e32929d19644fdd1e193774c20cce50615d40
NazPalUA <104204125+NazPalUA@users.noreply.github.com>
2023-03-08 23:39:10 +0200

    implemented infinite scroll at Home Page

---
+++ src/hooks/useMultiplePageApi.jsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from 'react'
+
+export default function useMultiplePageApi(initialUrl, initialData = []) {
+    const [data, setData] = useState(initialData)
+    const [url, setUrl] = useState(initialUrl)
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
+    const [page, setPage] = useState(1)
+    const [hasMore, setHasMore] = useState(true)
+
+    console.log(data)
+    const fetchData = async () => {
+        try {
+            setLoading(true)
+            const response = await fetch(url)
+            const responseData = await response.json()
+            setData(prevData => {
+                const newData = [...prevData, ...responseData.results];
+                const uniqueData = newData.reduce((acc, item) => {
+                    if (!acc.find(i => i.id === item.id)) {
+                        acc.push(item);
+                    }
+                    return acc;
+                }, []);
+                return uniqueData;
+            });
+            setHasMore(responseData.results.length > 0)
+            setPage(page + 1)
+        } catch (error) {
+            setError(error)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        setData(initialData)
+        setPage(1)
+        setUrl(initialUrl)
+
+    }, [])
+
+    useEffect(() => {
+        if (!url) return
+        fetchData()
+    }, [url])
+
+    return { data, loading, error, hasMore, page, setUrl }
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,43 +1,55 @@
-import React, { useEffect, useState } from "react"
+import React from "react"
+import InfiniteScroll from 'react-infinite-scroll-component'
 import { Link } from "react-router-dom"
 import { nanoid } from "nanoid"
 import Welcome from "../components/Welcome"
 import SearchBox from "../components/SearchBox"
 import MovieCard from "../components/MovieCard"
-import useFetch from "../hooks/useFetch"
+import useMultiplePageApi from "../hooks/useMultiplePageApi"
 import './HomePage.scss'
 
 export default function HomePage(props) {
 
     const BASE_URL = "https://api.themoviedb.org/3"
     const API_KEY = "e980138e09662908e00ccbeacd080b08"
-    const [data] = useFetch(`${BASE_URL}/trending/movie/week?api_key=${API_KEY}`)
 
-    const popular = !data ? [] : data.results.map(movie => {
+    let { data, hasMore, page, setUrl } = useMultiplePageApi(`${BASE_URL}/movie//popular?api_key=${API_KEY}&language=en-US&page=1`)
+    console.log(page)
+
+    const popular = !data ? [] : data.map(movie => {
         return (
             <li className="home-page__popular-item" key={nanoid()}>
                 <Link to={`/movie-page/${movie.id}`} className="home-page__link">
-                    <MovieCard 
+                    <MovieCard
                         className="home-page__movie-card"
                         movieId={movie.id}
                         title={movie.title}
                         path={movie.poster_path ? `https://image.tmdb.org/t/p/original${movie.poster_path}` : null}
                         year={movie.release_date.slice(0, 4)}
-                        rating={Math.round(movie.vote_average*10)}
+                        rating={Math.round(movie.vote_average * 10)}
                     />
                 </Link>
             </li>
         )
-    })  
+    })
 
     return (
         <div className={`home-page ${props.className}`}>
             <Welcome className="home-page__welcome" />
             <SearchBox className="home-page__search-box" />
             <h4 className="home-page__popular-title">Popular movies right now</h4>
-            <ul className="home-page__popular-list card-grid">
-                {popular}
-            </ul>
+
+            <InfiniteScroll
+                dataLength={data.length}
+                next={()=>setUrl(`${BASE_URL}/movie//popular?api_key=${API_KEY}&language=en-US&page=${page}`)}
+                hasMore={hasMore} 
+                loader={<h4>Loading...</h4>}
+                endMessage={<p>No more movies</p>}
+            >
+                <ul className="home-page__popular-list card-grid">
+                    {popular}
+                </ul>
+            </InfiniteScroll>
         </div>
     )
 }
