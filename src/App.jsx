ffe7a2c4d9351923d351525d8faf1b2c183ffff1
NazPalUA <104204125+NazPalUA@users.noreply.github.com>
2023-03-16 23:30:27 +0200

    refactored App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, {useContext, useEffect} from "react"
-import './App.scss'
 import { Routes, Route, useLocation } from "react-router-dom"
 import { ModalContext } from "./context/ModalContext"
 import HomePage from './pages/HomePage'
@@ -13,21 +12,23 @@ import Sidebar from './components/Sidebar'
 import MobileHeader from './components/MobileHeader'
 import Modal from './components/Modal'
 import AddToWatchlist from './components/AddToWatchlist'
+import './App.scss'
 
 
 function App() {
-
+	// Get the ModalContext values
 	const {isModalActive, setIsModalActive} = useContext(ModalContext)
 
+	// Handle the link click to scroll to top
 	function handleLinkClick() {
 		window.scrollTo(0, 0)
 	}
 	
+	// Get the current location and scroll to top when location changes
 	const { pathname } = useLocation()
-	useEffect(() => {
-		handleLinkClick()
-	}, [pathname])
+	useEffect(() => {handleLinkClick()}, [pathname])
 
+	// Add event listener for all links to handle click and scroll to top (it needs to make sure that scroll to top happens even if link lead to the current location)
 	document.querySelectorAll("a").forEach(link => link.addEventListener("click", handleLinkClick))
 
 	return (
