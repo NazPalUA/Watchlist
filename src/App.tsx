0f4ba462c5b087f01208fb21b42b6ae478f7e903
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-02-06 22:54:38 +0200

    Refactor handleLinkClick function and optimize link click event listener

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import { useEffect } from "react"
+import { useCallback, useEffect } from "react"
 import { useLocation } from "react-router-dom"
 import AppRoutes from "./components/AppRoutes"
 import Overlays from "./components/Overlays"
 
 function App() {
   // Handle the link click to scroll to top
-  function handleLinkClick() {
+  const handleLinkClick = useCallback(() => {
     window.scrollTo(0, 0)
-  }
+  }, [])
 
   // Get the current location and scroll to top when location changes
   const { pathname } = useLocation()
   useEffect(() => {
     handleLinkClick()
-  }, [pathname])
-
-  // Add event listener for all links to handle click and scroll to top (it needs to make sure that scroll to top happens even if link lead to the current location)
-  document
-    .querySelectorAll("a")
-    .forEach((link) => link.addEventListener("click", handleLinkClick))
+  }, [pathname, handleLinkClick])
 
   return (
     <>
