8581e2757d4e26380e2ce0e93d537c7ed51c2158
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-02-09 01:31:37 +0200

    Add UserLink component to Sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,42 +1,50 @@
+import "./Sidebar.scss"
 import SidebarLink from "./subComponents/SidebarLink"
-import manageSidebarOpening from "./utils/manageSidebarOpening"
+import UserLink from "./subComponents/UserLink/UserLink"
 import WatchlistsList from "./subComponents/WatchlistsList"
-import './Sidebar.scss'
+import manageSidebarOpening from "./utils/manageSidebarOpening"
 
 type SidebarPropTypes = { className?: string }
 
-function Sidebar({className}: SidebarPropTypes) {
+function Sidebar({ className }: SidebarPropTypes) {
+  manageSidebarOpening()
 
-    manageSidebarOpening()
+  return (
+    <div className={`sidebar ${className}`}>
+      <div className="sidebar__top">
+        <SidebarLink to="/" className="sidebar__logo-link">
+          <h1 className="sidebar__logo">Watchlists</h1>
+        </SidebarLink>
 
-    return (
-        <div className={`sidebar ${className}`}>
-            <div className="sidebar__top">
-                <SidebarLink to="/" className="sidebar__logo-link">
-                    <h1 className="sidebar__logo">Watchlists</h1>
-                </SidebarLink>
+        <nav className="sidebar__navigation">
+          <SidebarLink
+            to="/"
+            className="sidebar__navigation-link sidebar__navigation-link_home"
+          >
+            Home
+          </SidebarLink>
 
-                <nav className="sidebar__navigation">
-                    <SidebarLink to="/" className="sidebar__navigation-link sidebar__navigation-link_home" >
-                        Home
-                    </SidebarLink>
+          <SidebarLink
+            to="/history"
+            className="sidebar__navigation-link sidebar__navigation-link_history"
+          >
+            History
+          </SidebarLink>
+        </nav>
 
-                    <SidebarLink to="/history" className="sidebar__navigation-link sidebar__navigation-link_history" >
-                        History
-                    </SidebarLink>
-                </nav>
+        <SidebarLink to="/create_watchlist" className="sidebar__create-btn">
+          + Create watchlist
+        </SidebarLink>
+      </div>
 
-                <SidebarLink to="/create_watchlist" className="sidebar__create-btn" >
-                    +  Create watchlist
-                </SidebarLink>
-            </div>
+      <div className="sidebar__bottom">
+        <p className="sidebar__watchlists-header">My Lists</p>
+        <WatchlistsList />
+      </div>
 
-            <div className="sidebar__bottom">
-                <p className="sidebar__watchlists-header">My Lists</p>
-                <WatchlistsList />
-            </div>
-        </div>
-    )
+      <UserLink to="/user?action=create&id=43" />
+    </div>
+  )
 }
-    
+
 export default Sidebar
