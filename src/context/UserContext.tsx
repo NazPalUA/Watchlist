c25d6bc6a31b8ef45eedb57796a363b44ba41e56
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-03-03 01:47:20 +0200

    Refactor UserContext component

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,27 +1,49 @@
-// UserContext.tsx
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import { onAuthStateChanged } from "firebase/auth"
 import { doc, getDoc, setDoc } from "firebase/firestore"
 import React, { FC, ReactNode, createContext, useEffect, useState } from "react"
 import { auth } from "../services/firebase/firebase-auth"
 import { db } from "../services/firebase/firebase-config"
 
-interface User {
+type User = {
   uid: string
   email: string | null
   name: string | null
-  watchlistTitles: string[]
-  watchlists: {
-    title: string
-    description: string
-    watchlistId: string
-    createdAt: Date
-    movieIds: string[]
-  }[]
 }
 
-interface UserContextProps {
+const fetchUser = async (uid: string) => {
+  const docRef = doc(db, "users", uid)
+  const userDoc = await getDoc(docRef)
+  if (userDoc.exists()) {
+    return userDoc.data() as User
+  }
+  return null
+}
+
+export const useFetchUserQuery = (uid: string) => {
+  return useQuery({
+    queryKey: ["user"],
+    queryFn: () => fetchUser(uid),
+  })
+}
+
+const setUser = async (uid: string, user: User) => {
+  const docRef = doc(db, "users", uid)
+  await setDoc(docRef, user)
+}
+
+export const useSetUserMutation = (uid: string) => {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: (user: User) => setUser(uid, user),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [uid] })
+    },
+  })
+}
+
+type UserContextProps = {
   user: User | null
-  setUser: React.Dispatch<React.SetStateAction<User | null>>
 }
 
 const UserContext = createContext<UserContextProps | undefined>(undefined)
@@ -31,38 +53,35 @@ type UserContextProviderProps = {
 
 export const UserProvider: FC<UserContextProviderProps> = ({ children }) => {
   const [user, setUser] = useState<User | null>(null)
+  const { data: userData, refetch } = useFetchUserQuery(user?.uid || "")
+  const { mutate } = useSetUserMutation(user?.uid || "")
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, async (user) => {
-      if (user) {
-        const docRef = doc(db, "users", user.uid)
-        const userDoc = await getDoc(docRef)
-        if (userDoc.exists()) {
-          const userData = userDoc.data() as User
-          if (!userData.email) {
-            userData.email = user.email
-          }
-          if (!userData.name) {
-            userData.name = user.displayName
-          }
-          setUser(userData)
-        } else {
-          setDoc(docRef, {
-            uid: user.uid,
-            email: user.email,
-            name: user.displayName,
-          })
+    const unsubscribe = onAuthStateChanged(auth, async (firebaseUser) => {
+      if (firebaseUser) {
+        const newUser = {
+          uid: firebaseUser.uid,
+          email: firebaseUser.email,
+          name: firebaseUser.displayName,
         }
+        setUser(newUser)
+        refetch()
       } else {
         setUser(null)
       }
     })
 
     return () => unsubscribe()
-  }, [])
+  }, [refetch])
+
+  useEffect(() => {
+    if (!userData && user) {
+      mutate(user)
+    }
+  }, [userData, user, mutate])
 
   return (
-    <UserContext.Provider value={{ user, setUser }}>
+    <UserContext.Provider value={{ user: user ?? null }}>
       {children}
     </UserContext.Provider>
   )
