fc9de8488b2701b0ccc7155d2fc27d6b1312d3c8
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-03-06 12:19:06 +0200

    Refactor useGetWatchlistsDataQuery and useGetWatchlistDataQuery in queries.ts

--- src/services/firebase/firestore/queries.ts
+++ src/services/firebase/firestore/queries.ts
@@ -1,21 +1,39 @@
-import { useQuery } from "@tanstack/react-query"
-import { useAuth } from "../../../context/AuthContext"
+import { useQuery, useQueryClient } from "@tanstack/react-query"
+import { useEffect } from "react"
+import { auth } from "../firebase-auth"
 import { getWatchlistData, getWatchlistsData } from "./endPoints"
 
 export const useGetWatchlistsDataQuery = () => {
-  const { currentUser } = useAuth()
-  return useQuery({
+  const { currentUser } = auth
+  const queryClient = useQueryClient()
+
+  const watchlists = useQuery({
     queryKey: ["watchlists"],
     queryFn: () =>
       currentUser ? getWatchlistsData(currentUser.uid) : undefined,
+    enabled: !!currentUser?.uid,
   })
+
+  useEffect(() => {
+    if (watchlists.isSuccess && watchlists.data) {
+      watchlists.data.forEach((watchlist) => {
+        queryClient.setQueryData(["watchlists", watchlist.id], watchlist)
+      })
+    }
+  }, [watchlists.data])
+  return { ...watchlists }
 }
 
 export const useGetWatchlistDataQuery = (watchlistId: string) => {
-  const { currentUser } = useAuth()
+  const { currentUser } = auth
+
   return useQuery({
     queryKey: ["watchlists", watchlistId],
-    queryFn: () =>
-      currentUser ? getWatchlistData(currentUser.uid, watchlistId) : undefined,
+    queryFn: () => {
+      return currentUser
+        ? getWatchlistData(currentUser.uid, watchlistId)
+        : undefined
+    },
+    enabled: !!currentUser?.uid,
   })
 }
