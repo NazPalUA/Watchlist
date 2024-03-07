a653a9d5521f8b5877b8c8e35a8a0b27f48adf02
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-03-08 00:07:58 +0200

    Refactor AuthWithSocialMedia component to use a generic button class

--- src/components/AuthWithSocialMedia/AuthWithSocialMedia.tsx
+++ src/components/AuthWithSocialMedia/AuthWithSocialMedia.tsx
@@ -1,67 +1,39 @@
-import { useLocation, useNavigate } from "react-router-dom"
-import {
-  signInWithFacebook,
-  signInWithGithub,
-  signInWithGoogle,
-} from "../../services/firebase/firebase-auth"
+import { useAuthWithSocialMediaMutation } from "../../services/firebase/auth/mutations"
+import { SocialMediaProvider } from "../../services/firebase/auth/types"
+import ErrorMessage from "../ErrorMessage/ErrorMessage"
 import style from "./AuthWithSocialMedia.module.scss"
+import getLogoUrl from "./utils/getLogoUrl"
 
 type AuthWithSocialMediaProps = {
-  provider: "google" | "facebook" | "github"
+  provider: SocialMediaProvider
 }
 
 export default function AuthWithSocialMedia({
   provider,
 }: AuthWithSocialMediaProps) {
-  let navigate = useNavigate()
-  let location = useLocation()
-  let from = location.state?.from?.pathname || "/"
+  const {
+    mutate: authWithSocialMedia,
+    isError,
+    error,
+  } = useAuthWithSocialMediaMutation(provider)
 
-  function handleSubmit() {
-    let signIn
-    switch (provider) {
-      case "google":
-        signIn = signInWithGoogle
-        break
-      case "facebook":
-        signIn = signInWithFacebook
-        break
-      case "github":
-        signIn = signInWithGithub
-        break
-    }
-
-    try {
-      signIn().then(() => {
-        navigate(from, { replace: true })
-      })
-    } catch (error) {
-      console.error("Error signing in with social media", error)
-    }
-  }
-
-  const buttonStyle = style[`${provider}Button`]
   const buttonText = `Continue with ${
     provider.charAt(0).toUpperCase() + provider.slice(1)
   }`
 
-  let logoUrl
-  switch (provider) {
-    case "google":
-      logoUrl = "https://img.icons8.com/color/48/000000/google-logo.png"
-      break
-    case "facebook":
-      logoUrl = "https://img.icons8.com/color/48/000000/facebook-new.png"
-      break
-    case "github":
-      logoUrl = "https://img.icons8.com/color/48/000000/github--v1.png"
-      break
-  }
+  const logoUrl = getLogoUrl(provider)
 
   return (
-    <button className={buttonStyle} onClick={handleSubmit}>
-      <img src={logoUrl} alt={`${provider} logo`} />
-      <span>{buttonText}</span>
-    </button>
+    <>
+      <button className={style.button} onClick={() => authWithSocialMedia()}>
+        <img src={logoUrl} alt={`${provider} logo`} />
+        <span>{buttonText}</span>
+      </button>
+      {isError && (
+        <ErrorMessage error={error} isPopup={true}>
+          Something went wrong. Please try again later.
+        </ErrorMessage>
+      )}
+    </>
   )
 }
