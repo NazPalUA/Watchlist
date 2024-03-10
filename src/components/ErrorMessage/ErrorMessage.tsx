92123df7a1aee4500cd68376c99f0b90533fa0ce
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-03-11 01:59:18 +0200

    Add style props to ErrorMessage component

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -6,19 +6,31 @@ type ErrorMessageProps = {
   children: ReactNode
   error?: Error | null
   isPopup?: boolean
+  wrapperStyle?: React.CSSProperties
+  customMessageStyle?: React.CSSProperties
+  apiMessageStyle?: React.CSSProperties
 }
 
 export default function ErrorMessage({
   error,
   children,
   isPopup = false,
+  wrapperStyle,
+  customMessageStyle,
+  apiMessageStyle,
 }: ErrorMessageProps) {
   const [showError, setShowError] = useState(true)
 
   const Error = () => (
-    <div className={styles.error}>
-      <p className={styles.customMessage}>{children}</p>
-      {error && <p className={styles.apiMessage}>{error?.message}.</p>}
+    <div className={styles.error} style={wrapperStyle}>
+      <p className={styles.customMessage} style={customMessageStyle}>
+        {children}
+      </p>
+      {error && (
+        <p className={styles.apiMessage} style={apiMessageStyle}>
+          {error?.message}.
+        </p>
+      )}
     </div>
   )
 
