0f4f7b6a65e8b904e1774542ed4462c79749988f
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-03-11 02:47:45 +0200

    Refactor CreateWatchlistPage Form

--- src/pages/CreateWatchlistPage/CreateWatchlistPage.tsx
+++ src/pages/CreateWatchlistPage/CreateWatchlistPage.tsx
@@ -1,6 +1,7 @@
-import { ChangeEvent, FormEvent, useState } from "react"
 import { useNavigate } from "react-router-dom"
+import { getForm } from "../../components/Form"
 import { useCreateWatchlistMutation } from "../../services/firebase/firestore/mutations"
+import { TWatchlistSchema, watchlistSchema } from "../../types/form-watchlist"
 import "./CreateWatchlistPage.scss"
 
 type CreateWatchlistPageTypes = {
@@ -12,67 +13,32 @@ const CreateWatchlistPage: React.FC<CreateWatchlistPageTypes> = ({
   // Use the useNavigate hook from react-router to navigate to another page
   const navigate = useNavigate()
 
-  // Form state to store the name and description of the new watchlist
-  const [formData, setFormData] = useState({
-    name: "",
-    description: "",
-  })
-
   const { mutate: createWatchlist } = useCreateWatchlistMutation()
 
-  // Function to handle changes in the form
-  function handleChange(
-    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) {
-    const { name, value } = event.target
-    setFormData((prevWatchlistData) => ({
-      ...prevWatchlistData,
-      [name]: value,
-    }))
-  }
-
   // Function to handle the submit event of the form
-  function handleSubmit(event: FormEvent) {
-    event.preventDefault()
-
+  function handleSubmit(data: TWatchlistSchema) {
     // Call the createWatchlist function to add a new watchlist
-    createWatchlist(formData, {
+    createWatchlist(data, {
       onSuccess: (data) => {
         navigate(`/watchlist-page/${data?.id}`)
       },
     })
   }
-
+  const Form = getForm<TWatchlistSchema>()
   return (
     <div className={`create-watchlist-page ${className}`}>
       <h4 className="create-watchlist-page__title">Create a new watchlist</h4>
-      <form className="create-watchlist-page__form" onSubmit={handleSubmit}>
-        <label className="create-watchlist-page__label" htmlFor="name">
-          Name
-        </label>
-        <input
-          className="create-watchlist-page__name"
-          type="text"
-          id="name"
-          placeholder=""
-          onChange={handleChange}
-          name="name"
-          required
-          value={formData.name}
-        />
-        <label className="create-watchlist-page__label" htmlFor="description">
+      <Form
+        onSubmit={handleSubmit}
+        schema={watchlistSchema}
+        style={{ maxWidth: "unset" }}
+      >
+        <Form.Field name="name">Name</Form.Field>
+        <Form.Field name="description" type="textarea">
           Description
-        </label>
-        <textarea
-          className="create-watchlist-page__description"
-          id="description"
-          placeholder=""
-          onChange={handleChange}
-          name="description"
-          value={formData.description}
-        />
-        <button className="create-watchlist-page__btn">Create watchlist</button>
-      </form>
+        </Form.Field>
+        <Form.SubmitButton>Create watchlist</Form.SubmitButton>
+      </Form>
     </div>
   )
 }
