d5866a3c945f8571a9f63462e1c8a88ea911dc1d
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-03-13 02:10:16 +0200

    Refactor form-watchlist.ts to validate and transform input values

--- src/types/form-watchlist.ts
+++ src/types/form-watchlist.ts
@@ -3,9 +3,13 @@ import { z } from "zod"
 export const watchlistSchema = z.object({
   name: z
     .string()
-    .min(1, "Name can't be empty")
-    .max(50, "Name can't exceed 50 characters"),
-  description: z.string().max(500, "Description can't exceed 500 characters"),
+    .max(50, "Name can't exceed 50 characters")
+    .refine((value) => value.trim().length > 0, "Name can't be empty")
+    .transform((value) => value.trim()),
+  description: z
+    .string()
+    .max(500, "Description can't exceed 500 characters")
+    .transform((value) => value.trim()),
 })
 
 export type TWatchlistSchema = z.infer<typeof watchlistSchema>
