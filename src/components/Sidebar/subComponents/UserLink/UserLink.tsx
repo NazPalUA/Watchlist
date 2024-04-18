003fb19fe1361b9410662baf5878cbe10344f96d
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-04-18 20:34:08 +0300

    Refactor useGetUserQuery to use useGetUserDataQuery in UserLink.tsx and EditUserForm.tsx

--- src/components/Sidebar/subComponents/UserLink/UserLink.tsx
+++ src/components/Sidebar/subComponents/UserLink/UserLink.tsx
@@ -1,7 +1,7 @@
 import { FiMoreHorizontal } from "react-icons/fi"
 import { NavLink, NavLinkProps } from "react-router-dom"
 import guestIcon from "../../../../assets/images/guest_icon.svg"
-import { useGetUserQuery } from "../../../../services/firebase/auth/queries"
+import { useGetUserDataQuery } from "../../../../services/firebase/firestore/queries/queries"
 import useManageSidebarBasedOnWindowSize from "../../hooks/useManageSidebarBasedOnWindowSize"
 import style from "./UserLink.module.scss"
 
@@ -16,7 +16,7 @@ export default function UserLink({
 }: SidebarLinkProps) {
   const { toggleSidebarIfMobile } = useManageSidebarBasedOnWindowSize()
 
-  const { data: user } = useGetUserQuery()
+  const { data: userData } = useGetUserDataQuery()
 
   return (
     <NavLink
@@ -29,7 +29,7 @@ export default function UserLink({
       {...rest}
     >
       <img className={style.img} src={guestIcon} alt="guest icon" />
-      <p className={style.text}>{user?.displayName || "Guest"}</p>
+      <p className={style.text}>{userData?.name || "Guest"}</p>
       <FiMoreHorizontal className={style.btnIcon} color="#e1e1e1" size={32} />
     </NavLink>
   )
