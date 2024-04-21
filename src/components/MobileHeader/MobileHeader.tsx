ac337cd55c9ae33fe07ce5692624f9cbbdb002c7
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-04-21 23:08:59 +0300

    Refactor Burger component and MobileHeader component to use updated UI components

--- src/components/MobileHeader/MobileHeader.tsx
+++ src/components/MobileHeader/MobileHeader.tsx
@@ -1,23 +1,23 @@
 import { Link } from "react-router-dom"
-import Burger from "../Burger/Burger"
-import './MobileHeader.scss'
+import { useSidebarContext } from "../../context/SidebarContext"
+import Burger from "../UI/Burger/Burger"
+import "./MobileHeader.scss"
 
 type MobileHeaderPropTypes = {
-    className?: string
+  className?: string
 }
 
 function MobileHeader({ className }: MobileHeaderPropTypes) {
+  const { toggleSidebar, isSidebarOpen } = useSidebarContext()
 
-    return (
-        <nav className={`mobile-header ${className}`}>
-            <Link to="/" className="mobile-header__logo-link">
-                <h1 className="mobile-header__logo">
-                    Watchlists
-                </h1>
-            </Link>
-            <Burger className="mobile-header__burger" />
-        </nav>
-    )
+  return (
+    <nav className={`mobile-header ${className}`}>
+      <Link to="/" className="mobile-header__logo-link">
+        <h1 className="mobile-header__logo">Watchlists</h1>
+      </Link>
+      <Burger isOpen={isSidebarOpen} toggle={toggleSidebar} />
+    </nav>
+  )
 }
 
 export default MobileHeader
---
+++ src/components/ui/Burger/Burger.tsx
@@ -0,0 +1,19 @@
+import classNames from "classnames"
+import styles from "./Burger.module.scss"
+
+type BurgerPropTypes = {
+  isOpen: boolean
+  toggle: () => void
+}
+
+export default function Burger({ isOpen, toggle }: BurgerPropTypes) {
+  const burgerClasses = classNames(styles.burger, { [styles.open]: isOpen })
+
+  return (
+    <div className={burgerClasses} onClick={toggle}>
+      <div className={styles.bar}></div>
+      <div className={styles.bar}></div>
+      <div className={styles.bar}></div>
+    </div>
+  )
+}
