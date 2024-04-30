fa2244149d8bcd55a1460974df05a18c71e471d7
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-04-30 23:30:55 +0300

    Add console.log statement to addUserData function for debugging

--- src/services/firebase/firestore/mutations/endPoints.ts
+++ src/services/firebase/firestore/mutations/endPoints.ts
@@ -27,6 +27,8 @@ export const addUserData = async (
     const docSnap = await getDoc(docRef)
 
     if (!docSnap.exists()) {
+      console.log("Adding user data")
+
       await setDoc(
         docRef,
         {
