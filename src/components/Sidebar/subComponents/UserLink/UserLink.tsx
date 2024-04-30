893eb7b9b23be3b88742f22dff5403d298e713f2
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-04-30 16:08:28 +0300

    Add CustomLoader component to UserLink for displaying loading state

--- src/components/Sidebar/subComponents/UserLink/UserLink.tsx
+++ src/components/Sidebar/subComponents/UserLink/UserLink.tsx
@@ -2,6 +2,7 @@ import { FiMoreHorizontal } from "react-icons/fi"
 import { NavLink, NavLinkProps } from "react-router-dom"
 import guestIcon from "../../../../assets/images/guest_icon.svg"
 import { useGetUserDataQuery } from "../../../../services/firebase/firestore/queries/queries"
+import CustomLoader from "../../../CustomLoader"
 import useManageSidebarBasedOnWindowSize from "../../hooks/useManageSidebarBasedOnWindowSize"
 import style from "./UserLink.module.scss"
 
@@ -16,9 +17,13 @@ export default function UserLink({
 }: SidebarLinkProps) {
   const { toggleSidebarIfMobile } = useManageSidebarBasedOnWindowSize()
 
-  const { data: userData } = useGetUserDataQuery()
+  const { data: userData, isPending } = useGetUserDataQuery()
+  console.log(isPending)
+  console.log(userData)
 
-  return (
+  return isPending ? (
+    <CustomLoader />
+  ) : (
     <NavLink
       className={({ isActive }) =>
         isActive
@@ -29,7 +34,9 @@ export default function UserLink({
       {...rest}
     >
       <img className={style.img} src={guestIcon} alt="guest icon" />
+
       <p className={style.text}>{userData?.name || "Guest"}</p>
+
       <FiMoreHorizontal className={style.btnIcon} color="#e1e1e1" size={32} />
     </NavLink>
   )
