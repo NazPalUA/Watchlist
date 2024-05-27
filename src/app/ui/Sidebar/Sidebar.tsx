af0e3eb97ffb331db3d01bf209710533afd7a1de
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-05-27 23:26:14 +0300

    refactor: update Sidebar component

--- src/app/ui/Sidebar/Sidebar.tsx
+++ src/app/ui/Sidebar/Sidebar.tsx
@@ -1,17 +1,12 @@
 import CreateWatchlistNavButton from "../../../features/CreateWatchlist/ui/CreateWatchlistNavButton"
-import { useSidebarContext } from "../../../shared/context/SidebarContext"
-import { Burger } from "../../../widgets/HeaderNavbar/ui/Burger/Burger"
 import { NavBar } from "../../../widgets/NavBar"
 import { MyWatchlists } from "../../../widgets/WatchlistsList"
 import styles from "./Sidebar.module.scss"
 
 function Sidebar() {
-  const { toggleSidebar, isSidebarOpen } = useSidebarContext()
-
   return (
     <div className={styles.sidebar}>
       <div className={styles.topSection}>
-        <Burger isOpen={isSidebarOpen} toggle={toggleSidebar} />
         <NavBar />
         <CreateWatchlistNavButton />
       </div>
