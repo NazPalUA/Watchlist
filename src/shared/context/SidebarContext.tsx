d62e0b30c5812100da06d8e982a69ab1caa012dd
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-05-27 22:56:10 +0300

    refactor: Update SidebarContext to removed unused functions

--- src/shared/context/SidebarContext.tsx
+++ src/shared/context/SidebarContext.tsx
@@ -1,65 +1,64 @@
-import { useState, FC, createContext, useCallback, useContext } from "react"
+import { FC, createContext, useCallback, useContext, useState } from "react"
 
 export type SidebarContextType = {
-    isSidebarOpen: boolean,
-    openSidebar: () => void,
-    closeSidebar: () => void,
-    toggleSidebar: () => void,
-    getSidebarStateClass: () => string, // Function to get CSS class
+  isSidebarOpen: boolean
+  openSidebar: () => void
+  closeSidebar: () => void
+  toggleSidebar: () => void
 }
 
 const defaultContextValue: SidebarContextType = {
-    isSidebarOpen: false,
-    openSidebar: () => {},
-    closeSidebar: () => {},
-    toggleSidebar: () => {},
-    getSidebarStateClass: () => '' // Default implementation
+  isSidebarOpen: false,
+  openSidebar: () => {},
+  closeSidebar: () => {},
+  toggleSidebar: () => {},
 }
 
 const SidebarContext = createContext<SidebarContextType>(defaultContextValue)
 
 type SidebarContextProviderProps = {
-    children: React.ReactNode
+  children: React.ReactNode
 }
 
-const SidebarContextProvider: FC<SidebarContextProviderProps> = ({children}) => {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+const SidebarContextProvider: FC<SidebarContextProviderProps> = ({
+  children,
+}) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
 
-    const openSidebar = useCallback(() => {
-        setIsSidebarOpen(true)
-    }, [])
+  const openSidebar = useCallback(() => {
+    setIsSidebarOpen(true)
+  }, [])
 
-    const closeSidebar = useCallback(() => {
-        setIsSidebarOpen(false)
-    }, [])
+  const closeSidebar = useCallback(() => {
+    setIsSidebarOpen(false)
+  }, [])
 
-    const toggleSidebar = useCallback(() => {
-        setIsSidebarOpen(prevState => !prevState)
-    }, [])
+  const toggleSidebar = useCallback(() => {
+    setIsSidebarOpen((prevState) => !prevState)
+  }, [])
 
-    const getSidebarStateClass = useCallback(() => {
-        return isSidebarOpen ? 'sidebar-open' : 'sidebar-closed';
-    }, [isSidebarOpen]);
-
-    return (
-        <SidebarContext.Provider value={{
-            isSidebarOpen,
-            openSidebar,
-            closeSidebar,
-            toggleSidebar,
-            getSidebarStateClass
-        }}>
-            {children}
-        </SidebarContext.Provider>
-    )
+  return (
+    <SidebarContext.Provider
+      value={{
+        isSidebarOpen,
+        openSidebar,
+        closeSidebar,
+        toggleSidebar,
+      }}
+    >
+      {children}
+    </SidebarContext.Provider>
+  )
 }
 
 export const useSidebarContext = () => {
-    const context = useContext(SidebarContext);
-    if (!context) {
-        throw new Error("useSidebarContext must be used within a SidebarContextProvider");
-    }
-    return context;
+  const context = useContext(SidebarContext)
+  if (!context) {
+    throw new Error(
+      "useSidebarContext must be used within a SidebarContextProvider"
+    )
+  }
+  return context
 }
 
 export { SidebarContextProvider }
