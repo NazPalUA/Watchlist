d45b5a6c4881967e79dd02f30a546ea3d86de9c8
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-06-02 16:03:34 +0300

    refactor: Update fetchTmdbApi to handle error

--- src/shared/API/tmdb/api/api.ts
+++ src/shared/API/tmdb/api/api.ts
@@ -1,17 +1,23 @@
-import axios from 'axios'
+import axios from "axios"
 
 const apiKey = import.meta.env.VITE_TMDB_API_KEY
-const BASE_URL = 'https://api.themoviedb.org/3'
+const BASE_URL = "https://api.themoviedb.org/3"
 
 const axiosInstance = axios.create({
-    baseURL: BASE_URL,
-    params: {
-        api_key: apiKey,
-        language: 'en-US',
-    }
+  baseURL: BASE_URL,
+  params: {
+    api_key: apiKey,
+    language: "en-US",
+  },
 })
 
 // Function to fetch data from the TMDB API:
 export const fetchTmdbApi = async <T>(endpoint: string): Promise<T> => {
-    return (await axiosInstance.get<T>(endpoint)).data
+  try {
+    const response = await axiosInstance.get<T>(endpoint)
+    return response.data
+  } catch (error) {
+    console.error(`Error fetching data from TMDB API: ${error}`)
+    throw error
+  }
 }
