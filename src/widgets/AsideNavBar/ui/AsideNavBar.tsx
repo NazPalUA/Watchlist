b42d9060c41ea36cfcd337005c27386fa93ab6d8
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-06-07 10:40:25 +0300

    refactor: Add AsideNavLink to AsideNavBar

--- src/widgets/AsideNavBar/ui/AsideNavBar.tsx
+++ src/widgets/AsideNavBar/ui/AsideNavBar.tsx
@@ -1,5 +1,4 @@
-import { NavLinkWithActiveClass } from "../../../shared/ui/NavLinkWithActiveClass"
-import styles from "./AsideNavBar.module.scss"
+import { AsideNavLink } from "../../../shared/ui/AsideNavLink"
 import HistoryIcon from "/images/history_icon.svg"
 import HomeIcon from "/images/home_icon.svg"
 import WatchlistsIcon from "/images/watchlist_icon.svg"
@@ -9,29 +8,9 @@ type AsideNavBarProps = React.ComponentPropsWithoutRef<"nav">
 export function AsideNavBar({ ...rest }: AsideNavBarProps) {
   return (
     <nav {...rest}>
-      <NavLinkWithActiveClass
-        to="/"
-        className={`${styles.link} ${styles.link_home}`}
-      >
-        <img alt="home" src={HomeIcon} />
-        <span>Home</span>
-      </NavLinkWithActiveClass>
-
-      <NavLinkWithActiveClass
-        to="/watchlists"
-        className={`${styles.link} ${styles.link_watchlists}`}
-      >
-        <img alt="watchlists" src={WatchlistsIcon} />
-        <span>Watchlists</span>
-      </NavLinkWithActiveClass>
-
-      <NavLinkWithActiveClass
-        to="/history"
-        className={`${styles.link} ${styles.link_history}`}
-      >
-        <img alt="history" src={HistoryIcon} />
-        <span>History</span>
-      </NavLinkWithActiveClass>
+      <AsideNavLink to="/" icon={HomeIcon} text="Home" />
+      <AsideNavLink to="/watchlists" icon={WatchlistsIcon} text="Watchlists" />
+      <AsideNavLink to="/history" icon={HistoryIcon} text="History" />
     </nav>
   )
 }
