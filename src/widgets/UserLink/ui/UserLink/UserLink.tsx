6371251c89575390a44e1d0a9c267df2c15446dc
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-06-11 00:22:25 +0300

    add useOutsideClick to use User link

--- src/widgets/UserLink/ui/UserLink/UserLink.tsx
+++ src/widgets/UserLink/ui/UserLink/UserLink.tsx
@@ -1,17 +1,23 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
+import { useOutsideClick } from "../../lib/useOutsideClick"
 import { DropdownMenu } from "../DropdownMenu/DropdownMenu"
 import style from "./UserLink.module.scss"
 import guestIcon from "/images/guest_icon.svg"
 
 export function UserLink() {
   const [isOpen, setIsOpen] = useState(false)
+  const dropdownRef = useRef<HTMLDivElement>(null)
 
   const toggleMenu = () => {
     setIsOpen(!isOpen)
   }
 
+  useOutsideClick(dropdownRef, () => {
+    setIsOpen(false)
+  })
+
   return (
-    <nav className={style.container}>
+    <nav className={style.container} ref={dropdownRef}>
       <img
         onClick={toggleMenu}
         className={style.img}
