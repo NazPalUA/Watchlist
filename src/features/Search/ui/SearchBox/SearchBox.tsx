93bf4dfa3a34f7143c64a38d111eb9920f242c46
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-07-14 19:27:20 +0300

    refactor: Add Suspense in SearchBox component

--- src/features/Search/ui/SearchBox/SearchBox.tsx
+++ src/features/Search/ui/SearchBox/SearchBox.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { usePathname, useRouter, useSearchParams } from "next/navigation"
-import { ChangeEvent, useState } from "react"
+import { ChangeEvent, Suspense, useState } from "react"
 
 import { Button } from "../../../../shared/ui/Button"
 import "./SearchBox.scss"
@@ -43,18 +43,20 @@ export function SearchBox({ className }: SearchBoxPropTypes) {
   }
 
   return (
-    <div className={`search-box ${className}`}>
-      <input
-        className="search-box__input"
-        type="text"
-        name="search"
-        id="search-box__input"
-        placeholder="Search for movies by title"
-        onChange={handleChange}
-        onKeyDown={handleKeyDown}
-        value={inputText}
-      />
-      <Button onClick={handleClick}>search</Button>
-    </div>
+    <Suspense fallback={<div>Loading search box...</div>}>
+      <div className={`search-box ${className}`}>
+        <input
+          className="search-box__input"
+          type="text"
+          name="search"
+          id="search-box__input"
+          placeholder="Search for movies by title"
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
+          value={inputText}
+        />
+        <Button onClick={handleClick}>search</Button>
+      </div>
+    </Suspense>
   )
 }
